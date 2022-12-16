8bec8875538c4e80f07630cf5a82e6fb77cadecf
User <ibrahimsimsek93@gmail.com>
2022-12-16 11:27:31 -0500

    tst

--- src/test/java/com/krafttech/pages/pages/BasePage.java
+++ src/test/java/com/krafttech/pages/pages/BasePage.java
@@ -1,8 +1,8 @@
 package com.krafttech.pages.pages;
 
 import com.krafttech.utilities.BrowserUtils;
-import com.krafttech.utilities.Driver;
 
+import com.krafttech.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
